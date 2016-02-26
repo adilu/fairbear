ae7f7979e8b213bd92094a9bb16bf2fdecbfbf92
adilu <adrian.luethi@gmail.com>
2016-02-26 23:52:19 +0100

    Signed-off-by: adilu <adrian.luethi@gmail.com>

---
+++ main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+		$('.menu-button').click(function() {
+				$('#flyout-navigation').toggle("drop");
+			});
+			// MAKES MENU INTERACTIVE
+		$(window).on('orientationchange', function(){
+			if (window.orientation != 0) {
+				alert('Das Design dieser App ist für das Hochformat (Portrait-Modus) optimiert. Für die beste Darstellungsweise drehen Sie das Smartphone bitte wieder zurück.')
+			};	
+		});
+		//SHOULD GIVE AN ALERT WHEN THE PHONE IS TURNED FROM PORTRAIT TO LANDSCAPE MODE.
+		
+	});		
